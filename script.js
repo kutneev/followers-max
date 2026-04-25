5dab50820442f731b5fb7be6cc973d21668a12d5
Ruslan Kutneev <ruslan.kutneev@gmail.com>
2026-04-25 10:33:31 +0300

    Sync Telegram plans and refine step icon

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   var forms = document.querySelectorAll(".lead-form");
+  var planButtons = document.querySelectorAll("[data-plan-select]");
+
+  planButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      var planId = button.getAttribute("data-plan-select");
+      var targetInput = document.querySelector(
+        'input[data-plan-id="' + planId + '"]'
+      );
+
+      if (targetInput) {
+        targetInput.checked = true;
+      }
+    });
+  });
 
   forms.forEach(function (form) {
     form.addEventListener("submit", function (event) {
